48fe8b50b13e3716ae55774a0c4dca89aec09c1e
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-10-28 21:56:26 +0700

    daisy ui and hero layout

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,21 @@
 function IndexPage() {
   return (
     <>
-      <h1>Index Page</h1>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <p className="py-6">Task Management Reimagined</p>
+            <h1 className="text-5xl font-bold">Focus. Play. Achieve More.</h1>
+            <p className="py-6">
+              Transform your productivity with our minimal task manager that
+              makes work feel like play.
+            </p>
+            <button className="btn btn-dark btn-outline rounded-full">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

390845c9691f7f929ff94dc1e932c4d1671a6cb9
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-10-28 21:06:27 +0700

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactNode, useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
-function App() {
+function App(): ReactNode {
   const [count, setCount] = useState(0)
 
   return (
